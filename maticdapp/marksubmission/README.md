e334677fb94f4ba6e5c71c9550f41ba9686095ec
joel-razor1 <joelabraham8857@gmail.com>
2020-03-05 06:40:45 +0530

    voting application

---
+++ maticdapp/marksubmission/README.md
@@ -0,0 +1,31 @@
+# Decentralized Airbnb
+Setup
+
+1. Install dependencies
+```
+npm install
+```
+
+2. Follow along the instructions to complete the code.
+- Follow [instructions](./Instructions.md) to complete the smart contract code.
+- Follow [setup](./setup.md) to setup all requirements.
+- Follow [dapps](./dapps.md) to complete the DApp code.
+
+3. Run ethereum blockchain locally
+```
+npm run test:ethereum
+```
+
+4. Run DApp UI
+```
+cd dapp-ui
+npm install
+npm run build
+npm run start
+```
+
+5. Navigate to http://localhost:3000/ to see the app running.
+
+## Full Solution
+
+[Complete Decentralized Airbnb](https://github.com/maticnetwork/ethindia-workshop/tree/complete-dapp) | or `git checkout complete-dapp`
---
+++ maticdapp/marksubmission/dapp-ui/plugins/utils.js
@@ -0,0 +1,86 @@
+import AirbnbABI from "./airbnbABI";
+const Web3 = require("web3");
+
+let metamaskWeb3 = new Web3("http://localhost:8545");
+let account = null;
+let airbnbContract;
+let airbnbContractAddress = "0x04aC3C070D4faD9Ff1Da12BA4Fe08dA5AeC76a07"; // Paste Contract address here
+
+export function web3() {
+  return metamaskWeb3;
+}
+
+export const accountAddress = () => {
+  return account;
+};
+
+export async function setProvider() {
+  // TODO: get injected Metamask Object and create Web3 instance
+  if (window.ethereum) {
+    metamaskWeb3 = new Web3(ethereum);
+    try {
+      // Request account access if needed
+      await ethereum.enable();
+    } catch (error) {
+      // User denied account access...
+    }
+  } else if (window.web3) {
+    metamaskWeb3 = new Web3(web3.currentProvider);
+  }
+  account = await metamaskWeb3.eth.getAccounts();
+}
+
+function getAirbnbContract() {
+  airbnbContract =
+    airbnbContract ||
+    new metamaskWeb3.eth.Contract(AirbnbABI.abi, airbnbContractAddress);
+  return airbnbContract;
+}
+
+export async function postProperty(name, description, price) {
+  // TODO: call Airbnb.rentOutproperty
+  const prop = await getAirbnbContract()
+    .methods.rentOutproperty(name, description, price)
+    .send({
+      from: account[0]
+    });
+  alert("Property Posted Successfully");
+}
+
+export async function bookProperty(
+  spaceId,
+  checkInDate,
+  checkOutDate,
+  totalPrice
+) {
+  // TODO: call Airbnb.rentSpace
+  const prop = await getAirbnbContract()
+    .methods.rentProperty(spaceId, checkInDate, checkOutDate)
+    .send({
+      from: account[0],
+      value: totalPrice
+    });
+
+  alert("Property Booked Successfully");
+}
+
+export async function fetchAllProperties() {
+  const propertyId = await getAirbnbContract()
+    .methods.propertyId()
+    .call();
+  // iterate till property Id
+  const properties = [];
+  for (let i = 0; i < propertyId; i++) {
+    const p = await airbnbContract.methods.properties(i).call();
+    properties.push({
+      id: i,
+      name: p.name,
+      description: p.description,
+      price: metamaskWeb3.utils.fromWei(p.price)
+    });
+  }
+  return properties;
+  // TODO: call Airbnb.propertyId
+  // iterate till property Id
+  // push each object to properties array
+}
